5ffac312ccf802cd796072476006f53c3a898d66
Vitalii S. <svitalii138@gmail.com>
2024-11-20 12:31:17 +0200

    launch script added

--- src/app.js
+++ src/app.js
@@ -4,3 +4,17 @@ require("dotenv").config();
 
 const { APP_TOKEN, ADMIN_ID } = process.env;
 const bot = new Telegraf(APP_TOKEN);
+
+const orders = {};
+
+bot.start((ctx) => {
+  ctx.reply(
+    `Привіт, мене звати Connect Worker Bot. Я допоможу зробити тобі замовлення.\n\nЗаповни таблицю:`
+  );
+  ctx.reply(
+    `1. Кого шукаєш?\n2. Кількість людей?\n3. Скільки годин робота?\n4. Оплата (від 100грн/год)?\n5. Локація (місце роботи)?\n6. Ваш номер або нік Телеграм.`
+  );
+});
+
+bot.launch();
+console.log("Bot is running...");
