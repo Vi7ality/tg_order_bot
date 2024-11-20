11b30813f2e574c8807b8933be57835938fdfe50
Vitalii S. <svitalii138@gmail.com>
2024-11-20 12:18:15 +0200

    nodemon added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
 const { Telegraf } = require("telegraf");
 const path = require("path");
 require("dotenv").config();
+
+const { APP_TOKEN, ADMIN_ID } = process.env;
+const bot = new Telegraf(APP_TOKEN);
